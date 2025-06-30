4978b291d9311be4ec4e182336ba51874046dfc6
AlcidesDev809 <alcides.alipio.dev@gmail.com>
2025-06-30 09:45:01 +0100

    File Functions Update

--- include/file.h
+++ include/file.h
@@ -1,6 +1,17 @@
 #ifndef FILE_H
 #define FILE_H
 
+typedef struct
+{
+    FILE *file;
+    char nameFile[1024];
+    char pathFile[1024];
+} File;
+
 void getFilePath(char *buffer);
+
+void closeFile(File *fileStruct);
+
+int openFile(File *fileStruct, const char *mode);
 
 #endif // FILE_H
--- src/file.c
+++ src/file.c
@@ -1,26 +1,44 @@
 #include <windows.h>
 #include <shobjidl.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "file.h"
+
+int openFile(File *fileStruct, const char *mode)
+{
+    getFilePath(fileStruct->pathFile);
+    fileStruct->file = NULL;
+    fopen_s(&fileStruct->file, fileStruct->pathFile, mode);
+    if (fileStruct->file == NULL)
+        return 0;
+    return 1;
+}
+
+void closeFile(File *fileStruct)
+{
+    if (fileStruct->file != NULL)
+        fclose(fileStruct->file);
+}
 
 void getFilePath(char *buffer)
 {
     SetProcessDPIAware();
 
-    HRESULT hr = CoInitialize(NULL);
+    HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
     if (SUCCEEDED(hr))
     {
         IFileOpenDialog *pFileOpen = NULL;
-
         hr = CoCreateInstance(&CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER,
                               &IID_IFileOpenDialog, (void **)&pFileOpen);
 
         if (SUCCEEDED(hr))
         {
             DWORD dwOptions;
-            pFileOpen->lpVtbl->GetOptions(pFileOpen, &dwOptions);
-            pFileOpen->lpVtbl->SetOptions(pFileOpen, dwOptions | FOS_FILEMUSTEXIST);
+            if (SUCCEEDED(pFileOpen->lpVtbl->GetOptions(pFileOpen, &dwOptions)))
+            {
+                pFileOpen->lpVtbl->SetOptions(pFileOpen, dwOptions | FOS_FILEMUSTEXIST);
+            }
 
             hr = pFileOpen->lpVtbl->Show(pFileOpen, NULL);
             if (SUCCEEDED(hr))
@@ -30,13 +48,13 @@ void getFilePath(char *buffer)
                 if (SUCCEEDED(hr))
                 {
                     PWSTR pszFilePath;
-                    pItem->lpVtbl->GetDisplayName(pItem, SIGDN_FILESYSPATH, &pszFilePath);
-
-                    char path[1024];
-                    wcstombs(path, pszFilePath, sizeof(path));
-                    strcpy(buffer, path);
-
-                    CoTaskMemFree(pszFilePath);
+                    hr = pItem->lpVtbl->GetDisplayName(pItem, SIGDN_FILESYSPATH, &pszFilePath);
+                    if (SUCCEEDED(hr))
+                    {
+                        size_t outSize;
+                        wcstombs_s(&outSize, buffer, MAX_PATH, pszFilePath, _TRUNCATE);
+                        CoTaskMemFree(pszFilePath);
+                    }
                     pItem->lpVtbl->Release(pItem);
                 }
             }
@@ -44,7 +62,4 @@ void getFilePath(char *buffer)
         }
         CoUninitialize();
     }
-
-    fflush(stdout);
-    fflush(stdin);
 }
